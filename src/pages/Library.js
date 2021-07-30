6855b52c4cbad7ec0ad187478b44d7f7b79aa8ca
Maria Pigazzi <maria.pigazzi98@gmail.com>
2021-07-30 19:32:48 +0200

    first draft library page

--- src/pages/Library.js
+++ src/pages/Library.js
@@ -1,9 +1,36 @@
 import React from 'react'
+import "../css/Library.css"
 
 function Library() {
     return (
-        <div className="main-page">
-           <h1>Library</h1>
+        <div className="main-page" id="library-page">
+           <section id="library-header">
+               <div className="header-wrapper">
+                    <div className="buttons">
+                        <button type="button" className="primary-button">
+                            Subject/English
+                        </button>
+                        <button type="button" className="primary-button">
+                            Newest upload
+                        </button>
+                    </div>
+                    <div className="search">
+                    
+                        <button type="button" className="primary-button">
+                            Go                        
+                        </button>
+                    </div>
+               </div>
+           </section>
+           <section id="library-books">
+                <h2>Books</h2> 
+           </section>
+           <section id="library-videos">
+                <h2>Videos</h2>  
+           </section>
+           <section id="library-footer">
+                <h2>Materials &amp; Links</h2>   
+           </section>
         </div>
     )
 }
