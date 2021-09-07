9a1813d2255f7e849d5ef313cfd890a5c3316672
Maria Pigazzi <maria.pigazzi98@gmail.com>
2021-09-07 12:52:52 +0200

    subjects page skeleton

--- src/pages/Subjects.js
+++ src/pages/Subjects.js
@@ -1,8 +1,42 @@
 import React from "react"
+import '../css/Subjects.css'
+
 
 const Subjects = () => {
     return (
-        <div>Subjects</div>
+        <div className="page-wrapper">
+            <div className="page-container">
+                <ul className="subject-list">
+                    <li className="active">
+                        <button type="button">English</button>
+                    </li>
+                    <li>
+                        <button type="button">Chemistry</button>
+                    </li>
+                    <li>
+                        <button type="button">Maths</button>
+                    </li>
+                    <li>
+                        <button type="button">Geography</button>
+                    </li>
+                    <li>
+                        <button type="button">Spanish</button>
+                    </li>
+                    <li>
+                        <button type="button">Literature</button>
+                    </li>
+                    <li>
+                        <button type="button">PE</button>
+                    </li>
+                    <li>
+                        <button type="button">History</button>
+                    </li>
+                    <li>
+                        <button type="button">Biology</button>
+                    </li>
+                </ul>
+            </div>
+        </div>
     )
 }
 
